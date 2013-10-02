6fa17b8d854af54483fd4c57447694c0a27876b2
Yuki Iwanaga <yuki@creasty.com>
2013-10-02 19:03:45 +0900

    added configuration files for ack & rails

--- install.sh
+++ install.sh
@@ -1,36 +1,52 @@
 #!/usr/bin/env bash
 
-cd ~/
-
 # install git
+echo "Checking git..."
 which git > /dev/null 2>&1
 if [ $? -eq 0 ] ; then
-  echo Success!
+  echo "ok"
 else
-  echo Install git! >&2
-  # sudo yum install -y git
+  echo "Installing git..."
   sudo brew install git
+  echo "ok"
 fi
 
 # install vim
+echo "Checking vim..."
 which vim > /dev/null 2>&1
 if [ $? -eq 0 ] ; then
-  echo Success!
+  echo "ok"
 else
-  echo Install vim! >&2
-  # sudo yum install -y vim-enhanced
+  echo "Installing vim..."
   sudo brew install vim
+  echo "ok"
 fi
 
-# clone
-git clone --recursive git@github.com:creasty/dotfiles.git
+# clone / update
+if [ -d .git ]; then
+  echo "Cloning repo..."
+  cd ~
+  git clone --recursive git@github.com:creasty/dotfiles.git
+else
+  echo "Updating repo..."
+  cd ~/dotfiles
+  git pull origin master
+fi
+echo "ok"
 
 # symbolic links
+echo "Creating symbolic links..."
+cd ~
 ln -s ~/dotfiles/vimfiles ~/.vim
 ln -s ~/dotfiles/_vimrc ~/.vimrc
 ln -s ~/dotfiles/_gitconfig ~/.gitconfig
 ln -s ~/dotfiles/_gitignore ~/.gitignore
+ln -s ~/dotfiles/_railsrc ~/.railsrc
+ln -s ~/dotfiles/_ackrc ~/.ackrc
+echo "ok"
 
 # neo bundle
+echo "Updating vim plugin..."
 vim +NeoBundleInstall +qa
+echo "ok"
 
