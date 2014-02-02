f245737dbe01dbe770f70ffddcf9ea75b691e9cf
Yuki Iwanaga <yuki@creasty.com>
2014-02-02 13:26:32 +0900

    Nodejs packages

---
+++ setups/nodejs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+install() {
+  npm i -g "$@"
+}
+
+install bower
+install cheerio
+install coffee-script
+install coffeelint
+install growl
+install grunt-cli
+install http-server
+install markdown
+install md2html
+install mocha
+install uglify-js
+install underscore
+install yeoman-generator
+install yo
+
