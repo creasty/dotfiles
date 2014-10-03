5a580be80023d0bb892b14002d1c1885cbbd5b89
Yuki Iwanaga <yuki@creasty.com>
2014-10-03 20:30:40 +0900

    Include guard

--- script/helpers.sh
+++ script/helpers.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+[ -n "$DOTFILES_HELPER_LOADED" ] && exit
+
+export DOTFILES_HELPER_LOADED=1
+
 section() {
   printf "\e[33m==> $1\e[0m\n"
 }
 
 subsection() {
-  printf "\e[37m--- $1\e[0m\n"
+  printf "\e[37m--> $1\e[0m\n"
 }
 
 show_and_run() {
