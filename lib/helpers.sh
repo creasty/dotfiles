2a7c9b6dac473dcf32fcc3500ab21b0b23a0077f
Yuki Iwanaga <yuki@creasty.com>
2014-11-16 01:20:22 +0900

    Fixing bootstrap

--- lib/helpers.sh
+++ lib/helpers.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+
+#  Section
+#-----------------------------------------------
 section() {
   printf "\e[33m==> $1\e[0m\n"
 }
@@ -8,11 +11,9 @@ subsection() {
   printf "\e[37m--> $1\e[0m\n"
 }
 
-show_and_run() {
-  echo "$@"
-  exec "$@"
-}
 
+#  Ask
+#-----------------------------------------------
 ask() {
   echo -n "$1"
   read REPLY
@@ -28,4 +29,19 @@ answer_is_yes() {
   else
     return 1
   fi
+}
+
+
+#  Print labeled text
+#-----------------------------------------------
+print_error() {
+  printf "\e[0;31m[Error]\e[0m $@\n"
+}
+
+print_success() {
+  printf "\e[0;32m[Success]\e[0m $@\n"
+}
+
+print_info() {
+  printf "\e[0;35m[Info]\e[0m $@\n"
 }
