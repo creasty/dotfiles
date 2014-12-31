f9256665b70054b4e00f74d576eff12d5f7cff92
Yuki Iwanaga <yuki@creasty.com>
2015-01-01 00:40:58 +0900

    Tweak

--- _zsh/aliases.zsh
+++ _zsh/aliases.zsh
@@ -28,7 +28,7 @@ alias o='git ls-files | peco | xargs open'
 
 alias sha1='echo -n "${1}" | openssl sha1 | sed "s/^.* //"'
 
-alias skey='ssh-keygen -t rsa -P "" -f'
+alias skey='ssh-keygen -q -b 2048 -t rsa -N "" -C "" -f'
 alias randportn='ruby -e "p rand(1024..65535)"'
 alias randports='ruby -e "p rand(49152..65535)"'
 
