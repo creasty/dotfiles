4bad29db28be852a98acc95ec7cc170190555124
Yuki Iwanaga <yuki@creasty.com>
2015-05-03 20:57:10 +0900

    Version file template for gem

---
+++ _vim/template/ruby/lib/template/version.rb
@@ -0,0 +1,13 @@
+<%-
+@module_name = File.basename(File.dirname('FILE_PATH'))
+  .gsub(/(_|\b)([a-z0-9])/) { $2.upcase }
+-%>
+module <%= @module_name %>
+
+  MAJOR    = 0
+  MINOR    = 0
+  REVISION = 1
+
+  VERSION = [MAJOR, MINOR, REVISION].compact.join(".")
+
+end
