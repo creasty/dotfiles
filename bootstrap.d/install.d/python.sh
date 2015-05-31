2b09a89baa081dfa6fd7bc6cd37d2b01162892e8
Yuki Iwanaga <yuki@creasty.com>
2015-05-31 12:53:34 +0900

    Extract python requirements

--- bootstrap.d/install.d/python.sh
+++ bootstrap.d/install.d/python.sh
@@ -1,8 +1,3 @@
 section "Installing Python packages"
 
-pip install --upgrade howdoi
-pip install --upgrade locustio
-pip install --upgrade mitmproxy
-pip install --upgrade numpy
-pip install --upgrade protobuf
-pip install --upgrade pyamf
+cat $DOTFILES_PATH/lua/requirements.txt | each pip install --upgrade
