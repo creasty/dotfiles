908262e63fc123ec116a059a84eecc243fcb1bfd
Yuki Iwanaga <yuki@creasty.com>
2015-06-13 21:09:30 +0900

    Set default version of nodejs

--- bootstrap.d/install.d/nodejs.sh
+++ bootstrap.d/install.d/nodejs.sh
@@ -7,10 +7,13 @@ else
   curl https://raw.githubusercontent.com/creationix/nvm/v0.17.3/install.sh | bash
 fi
 
-subsection "Installing nvm"
-nvm install 0.10
+subsection "Installing nodejs"
+nvm install 0.10.33
 print_status $?
 
-section "Installing nodejs packages"
+subsection "Set default version"
+nvm alias default 0.10.33
+nvm use default
 
+section "Installing nodejs packages"
 (cd $DOTFILES_PATH/nodejs && npm i -g)
