ebd7936786af9f988cd452a5ec425a7d2ea5a846
Yuki Iwanaga <yuki@creasty.com>
2015-08-03 20:20:23 +0900

    Fix

--- bootstrap.d/install.d/homebrew.sh
+++ bootstrap.d/install.d/homebrew.sh
@@ -7,5 +7,77 @@ else
   print_status $?
 fi
 
-section "Installing brew packages"
-source $DOTFILES_PATH/Brewfile
+section "Update formula"
+brew update
+
+
+section "Upgrade installed packages"
+brew upgrade
+
+
+section "Add repositories"
+cat $DOTFILES_PATH/brew/tap.txt \
+  | sed 's/\s*#.*$//' \
+  | {
+    while read -r line; do
+      if ! [ -z "$line" ]; then
+        subsection "Tapping $line"
+        brew tap $line || true
+      fi
+    done
+  }
+
+
+section "Install applications"
+
+subsection "Installing cask"
+brew install caskroom/cask/brew-cask || true
+
+cat $DOTFILES_PATH/brew/cask.txt \
+  | sed 's/\s*#.*$//' \
+  | {
+    while read -r line; do
+      if ! [ -z "$line" ]; then
+        subsection "Installing $line"
+        brew cask install $line || true
+      fi
+    done
+  }
+
+
+section "Fixate versions"
+PREFIX=$(brew --prefix)
+cat $DOTFILES_PATH/brew/version_fixations.txt \
+  | sed 's/\s*#.*$//' \
+  | {
+    while read -r line; do
+      if ! [ -z "$line" ]; then
+        version="$(cut -d' ' -f 1 <<< "$line")"
+        path="$(cut -d' ' -f 2 <<< "$line")"
+
+        git --gitdir=$PREFIX checkout $version -- $PREFIX/Library/$path
+      fi
+    done
+  }
+
+
+section "Install packages"
+cat $DOTFILES_PATH/brew/packages.txt \
+  | sed 's/\s*#.*$//' \
+  | {
+    while read -r line; do
+      if ! [ -z "$line" ]; then
+        subsection "Installing $line"
+        brew install $line || true
+      fi
+    done
+  }
+
+
+section "Link applications"
+brew cask alfred link
+brew linkapps
+
+
+section "Remove outdated versions"
+brew cleanup
