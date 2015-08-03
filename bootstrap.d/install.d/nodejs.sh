589529de25c65abc8e56419e52657f2f5ab1a477
Yuki Iwanaga <yuki@creasty.com>
2015-08-03 18:29:16 +0900

    Fix node installation

--- bootstrap.d/install.d/nodejs.sh
+++ bootstrap.d/install.d/nodejs.sh
@@ -1,3 +1,7 @@
+DEFAULT_VERSION="$(< $DOTFILES_PATH/.node-version)"
+
+INSTALLED_NODE_VERSIONS="$(node ls)"
+
 section "Installing nodejs"
 
 subsection "Setup nvm"
@@ -8,13 +12,25 @@ if ! [ -d "$NVM_DIR" ]; then
   ln -sf "$(brew --prefix nvm)" "$NVM_DIR"
 fi
 
-subsection "Installing nodejs"
-nvm install 0.10.33
-print_status $?
+
+_install_nodejs() {
+  local version=$1
+
+  subsection "Install $version"
+
+  if [ -z "$(echo -n "$INSTALLED_NODE_VERSIONS" | grep $version)" ]; then
+    nvm install $version
+    print_status $?
+  else
+    print_info "Installed"
+  fi
+}
+
+cat $DOTFILES_PATH/nodejs/packages.txt | xargs -n 1 _install_nodejs
 
 subsection "Set default version"
-nvm alias default 0.10.33
+nvm alias default $DEFAULT_VERSION
 nvm use default
 
 section "Installing nodejs packages"
-(cd $DOTFILES_PATH/nodejs && npm i -g)
+cat $DOTFILES_PATH/nodejs/packages.txt | xargs -n 1 npm i -g
