66d911e9bbab17532fe3d210a3b51d9fd0a50cde
Yuki Iwanaga <yuki@creasty.com>
2016-02-18 19:46:37 +0900

    Set env vars from docker-machine env

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -52,16 +52,6 @@ export GIT_MERGE_AUTOEDIT='no'
 export DISABLE_SPRING=1
 
 
-#  Docker
-#-----------------------------------------------
-# $ docker-machine create --driver virtualbox local
-# $ docker-machine env local
-export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="tcp://192.168.99.100:2376"
-export DOCKER_CERT_PATH="$HOME/.docker/machine/machines/local"
-export DOCKER_MACHINE_NAME="local"
-
-
 #  Iruka
 #-----------------------------------------------
 export IRUKA_MACHINE=172.17.8.101
