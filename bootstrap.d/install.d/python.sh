e56086eb575ffdbcde39b211ae358c80735573ed
Yuki Iwanaga <yuki@creasty.com>
2016-02-27 14:51:28 +0900

    Use pip3

--- bootstrap.d/install.d/python.sh
+++ bootstrap.d/install.d/python.sh
@@ -7,4 +7,4 @@ print_status "Installed"
 
 section "Installing Python packages"
 
-cat $DOTFILES_PATH/lua/requirements.txt | xargs -n 1 pip install --upgrade
+cat $DOTFILES_PATH/lua/requirements.txt | xargs -n 1 pip3 install --upgrade
