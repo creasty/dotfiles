c86365e4a6fffba481429e0dfd218663f81cc6c2
Yuki Iwanaga <yuki@creasty.com>
2019-03-17 11:20:52 +0900

    Write minisnip completer

---
+++ vim/rplugin/python3/deoplete/sources/minisnip.py
@@ -0,0 +1,31 @@
+import os
+import re
+
+from .base import Base
+
+class Source(Base):
+
+    def __init__(self, vim):
+        Base.__init__(self, vim)
+
+        self.name = 'minisnip'
+        self.mark = '[minisnip]'
+        self.min_pattern_length = 0
+        self.minisnip_dir = self.vim.eval('g:minisnip_dir')
+        self.snippets = os.listdir(os.path.expanduser(self.minisnip_dir))
+
+    def gather_candidates(self, context):
+        ft = context['filetype']
+        ft_reg = '^_(' + format(ft.replace('.', '|')) + ')_'
+
+        candidates = []
+
+        for snippet in self.snippets:
+            if snippet.startswith('_'):
+                m = re.search(ft_reg, snippet)
+                if m:
+                    candidates.append(snippet[len(m.group(0)):])
+            else:
+                candidates.append(snippet)
+
+        return [{'word': c} for c in candidates]
