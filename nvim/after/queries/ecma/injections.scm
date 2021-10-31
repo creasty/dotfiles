27fcf47cd601dd4ec1771eef90bd777fed64d0c3
Yuki Iwanaga <yuki@creasty.com>
2021-10-31 21:20:37 +0900

    Update styled-components queries

--- nvim/after/queries/ecma/injections.scm
+++ nvim/after/queries/ecma/injections.scm
@@ -1,11 +1,37 @@
-(call_expression
-  (member_expression
-    (identifier) @_styled
-    (#eq? @_styled "styled"))
-  (template_string) @css)
+; @see https://github.com/nvim-treesitter/nvim-treesitter/pull/1628
 
+; styled.div`<css>`
 (call_expression
-  (call_expression
-    (identifier) @_styled
-    (#eq? @_styled "styled"))
-  (template_string) @css)
+ function: (member_expression
+   object: (identifier) @_name
+     (#eq? @_name "styled"))
+ arguments: ((template_string) @css
+   (#offset! @css 0 1 0 -1)))
+
+; styled(Component)`<css>`
+(call_expression
+ function: (call_expression
+   function: (identifier) @_name
+     (#eq? @_name "styled"))
+ arguments: ((template_string) @css
+   (#offset! @css 0 1 0 -1)))
+
+; styled.div.attrs({ prop: "foo" })`<css>`
+(call_expression
+ function: (call_expression
+   function: (member_expression
+    object: (member_expression
+      object: (identifier) @_name
+        (#eq? @_name "styled"))))
+ arguments: ((template_string) @css
+   (#offset! @css 0 1 0 -1)))
+
+; styled(Component).attrs({ prop: "foo" })`<css>`
+(call_expression
+ function: (call_expression
+   function: (member_expression
+    object: (call_expression
+      function: (identifier) @_name
+        (#eq? @_name "styled"))))
+ arguments: ((template_string) @css
+   (#offset! @css 0 1 0 -1)))
