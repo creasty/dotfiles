b90c5ea7ccf7f76ec78ad1a8f21c11a988da5743
Yuki Iwanaga <yuki@creasty.com>
2024-05-01 11:00:30 +0900

    Update templates

--- nvim/template/typescript/resolvers/queries/template.ts
+++ nvim/template/typescript/resolvers/queries/template.ts
@@ -1,5 +1,5 @@
 import { Resolver } from "resolvers/type";
 
 export const FILE_BASE_NAME: Resolver<"FILE_BASE_NAME"> = async (_parent, args, context, _info) => {
-  <+CURSOR+>
+  return context.api.<+CURSOR+>().FILE_BASE_NAME(<%= "FILE_BASE_NAME" %>RequestInputToPb(args.input)).then(<%= "FILE_BASE_NAME" %>ResponseToType);
 };
