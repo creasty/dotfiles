95aed6aacebca86d2ee758ac770793b7ec7564a6
Yuki Iwanaga <yuki@creasty.com>
2025-04-09 19:01:49 +0900

    Fix

--- nvim/lua/user/plugin/treesitter/init.lua
+++ nvim/lua/user/plugin/treesitter/init.lua
@@ -78,10 +78,6 @@ require('nvim-treesitter.configs').setup {
       node_incremental = 'gs',
     },
   },
-  -- windwp/nvim-ts-autotag
-  autotag = {
-    enable = true,
-  },
   -- yioneko/nvim-yati
   yati = {
     enable = true,
@@ -100,17 +96,25 @@ require('nvim-treesitter.configs').setup {
 require('syntax-tree-surfer')
 
 require('treesitter-context').setup {
-  enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
-  multiwindow = false,      -- Enable multiwindow support.
-  max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit.
-  min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+  enable = true,           -- Enable this plugin (Can be enabled/disabled later via commands)
+  multiwindow = false,     -- Enable multiwindow support.
+  max_lines = 0,           -- How many lines the window should span. Values <= 0 mean no limit.
+  min_window_height = 0,   -- Minimum editor window height to enable context. Values <= 0 mean no limit.
   line_numbers = true,
   multiline_threshold = 1, -- Maximum number of lines to show for a single context
-  trim_scope = 'outer',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-  mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
+  trim_scope = 'outer',    -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+  mode = 'cursor',         -- Line used to calculate context. Choices: 'cursor', 'topline'
   -- Separator between context and content. Should be a single character string, like '-'.
   -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
   separator = nil,
   zindex = 20,     -- The Z-index of the context window
   on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+}
+
+require('nvim-ts-autotag').setup {
+  opts = {
+    enable_close = true,
+    enable_rename = true,
+    enable_close_on_slash = true,
+  },
 }
